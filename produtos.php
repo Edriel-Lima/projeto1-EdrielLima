ce3c16a2d4fd7c17ff02195043067199c5169793
Edriel-Lima <edriellima04@gmail.com>
2022-09-12 16:49:47 -0300

    corrigido bug

--- produtos.php
+++ produtos.php
@@ -20,7 +20,7 @@ function produtos($id) {
     //tratando id recebido
     $id = numero($id);
     //buscando informacoes do produto no db
-    $db_produto = mysqli_featch_array(mysqli_query($conn, "SELECT id,nome FROM produtos WHERE id='".$id."'"));
+    $db_produto = mysqli_fetch_array(mysqli_query($conn, "SELECT id,nome FROM produtos WHERE id='".$id."'"));
 
     $nome_produto = $db_produto["nome"];
 
